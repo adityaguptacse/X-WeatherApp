694f5d0789f278a107d0e1b8fbc3267a3131f0cc
Aditya Gupta <adityaguptacse@gmail.com>
2024-04-27 16:47:38 -0700

    weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
+//SearchBar component
+const SearchBar = ({ onSearch }) => {
+  const [city, setCity] = useState("");
+
+  const handleSearch = () => {
+    onSearch(city);
+  };
+
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        placeholder="Enter city name"
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+
+// WeatherCard component
+
+const WeatherCard = ({title, data}) => {
+
+  return(
+    <div className="weather-card">
+      <h3>{title}</h3>
+      <p>{data}</p>
+    </div>
+  )
+}
+//WeatherDisplay component
+
+const WeatherDisplay = ({ city }) => {
+  const [weatherData, setWeaterData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (city) {
+      setLoading(true);
+      axios
+        .get("https://api.weatherapi.com/v1/current.json", {
+          params: {
+            key: "b60c9247b8bc4dc493764805242704",
+            q: city,
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          setWeaterData(res.data);
+        })
+        .catch((err) => {
+          console.error("Error fetching data");
+          alert("Failed to fetch weather data");
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+  }, [city]);
+
+  return (
+    <div className="weather-display">
+      {loading && <p>Loading data...</p>}
+
+      {!loading && weatherData && (
+        <div className="weather-cards">
+          <WeatherCard 
+          title="Temperature" 
+          data={`${weatherData.current.temp_c}°c`}          
+          />
+          <WeatherCard 
+          title="Humidity" 
+          data={`${weatherData.current.humidity}%`} 
+          />
+          <WeatherCard 
+          title="Condition" 
+          data={weatherData.current.condition.text}        
+          />
+            <WeatherCard 
+          title="Wind Speed" 
+          data={`${weatherData.current.wind_kph}kph`}          
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+
+
+//App component
 function App() {
+  const [city, setCity] = useState("");
+
+  const handleSearch = (searchCity) => {
+    setCity(searchCity);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar onSearch={handleSearch} />
+      <WeatherDisplay city={city} />
     </div>
   );
 }
